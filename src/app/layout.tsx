2a3f9e4dbb2e67cd2a566c1a56a55588d5e70708
Aleksi <liukkonenaleksi@gmail.com>
2024-09-07 07:30:59 +0300

    Edit layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "QuickPoll - Create and Share Polls Instantly",
@@ -25,13 +14,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-    <body
-  className={`flex justify-center items-center min-h-screen`}
->
-  <main className="w-full max-w-4xl p-4">
-    {children}
-  </main>
-</body>
+      <body className="flex justify-center items-center min-h-screen">
+        <main className="w-full max-w-4xl p-4">
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
