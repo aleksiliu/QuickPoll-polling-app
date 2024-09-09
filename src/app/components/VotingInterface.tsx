9af34bb5ea3bd434d45cac7b5eb348da841de45b
Aleksi <liukkonenaleksi@gmail.com>
2024-09-09 17:52:25 +0300

    Update share poll

--- src/app/components/VotingInterface.tsx
+++ src/app/components/VotingInterface.tsx
@@ -182,10 +182,10 @@ export default function VotingInterface({ pollId }: { pollId: string }) {
       <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1 text-purple-500" viewBox="0 0 20 20" fill="currentColor">
         <path d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z" />
       </svg>
-      Share
+      Share poll
     </button>
     {showTooltip && (
-      <div className="absolute top-full left-1/2 transform -translate-x-1/2 mt-2 px-3 py-1 bg-gray-800 text-white text-xs rounded shadow-lg transition-opacity duration-300">
+      <div className="absolute top-full mt-2 px-3 py-1 bg-gray-800 text-white text-xs rounded shadow-lg transition-opacity duration-300 text-center">
         Link added to clipboard!
       </div>
     )}
